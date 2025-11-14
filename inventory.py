2963dae4c65c963bca45f08e14e26a04a1f171e4
SandmanMaybeTe23 <billt.burlin@elev.ga.ntig.se>
2025-11-14 11:17:21 +0100

    looper

--- inventory.py
+++ inventory.py
@@ -1,3 +1,5 @@
+
+
 def input_int(prompt):
     while True:
         try:
@@ -5,66 +7,129 @@ def input_int(prompt):
         except ValueError:
             print("Invalid input. Please enter a number.")
 
+def input_str(prompt):
+    while True:
+        try:
+            return str(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a Word.")
 
 
-class inv():
+
+def make_item():
+        name_item=input_str("what is this item called")
+        weight_item=input_int(f"how heavy is this {name_item} ")    
+        value_item=input_int(f"how valuable is the {name_item}")
+        amount_item=input_int(f"how much of this {name_item} do you have")
+
+        name_item
+        return 
+        
+
+class Inv():
     def __init__(self):
         self.container = []
         self.weight= 0
 
     def get_contents(self):
-        return self.container
+        item=[]
+        for i in self.container:
+            item.append(i.name)
+        return  item
 
     def get_weight(self):
         return self.weight
 
 
         
-
         
-
     def add_item(self,item):
         self.container.append(item)
-        self.weight += item["weight"]
+        self.weight+= item.weight
+        
+        
 
     def remove_item(self,item):
         self.container.remove(item)
         self.weight -= item["weight"]
 
 
-class item():
-    def __init__(self,name ,weight,value,amount):
+
+class Item():
+    def __init__(self,name,weight,value,amount):
         self.name=name
         self.weight=weight
         self.value=value
         self.amount=amount
 
 
-        
-
-    
-
     def print_stats(self):
         print("item name: ", self.name)
         print("item weight: ", self.weight)
         print("item value: ", self.value)
         print("item amount: ", self.amount)
-        
-
-
-
     
+    def make_item(self):
+        self.name=input_str("what is this item called")
+        self.weight=input_int(f"how heavy is this {self.name} ")    
+        self.value=input_int(f"how valuable is the {self.name}")
+        self.amount=input_int(f"how much of this {self.name} do you have")
 
-player = inv()
 
-tomato =item(10,5,1)
 
-tomato.new_item()
+
+player = Inv()
+
+
+tomato = Item("tomato",10,5,1)
+
+
 
 tomato.print_stats()
 
+
 player.add_item(tomato)
+
+player.add_item(tomato)
+
+player.add_item(tomato)
+
+player.add_item(tomato)
+
+player.add_item(tomato)
+
+
+player.add_item(tomato)
+
+
+player.add_item(tomato)
+
+
 print(player.get_contents())
+
+
+print(player.get_weight())
+
+
+while True :
+    
+    new_item= Item(None,None,None,None)
+
+    new_item.make_item()
+
+    new_item.print_stats()
+
+    player.add_item(new_item)
+
+    print(player.get_contents())  
+
+    if  10 == len(player.get_contents()) :
+        break     
+    else:
+        pass
+
+
+
 
 
 
